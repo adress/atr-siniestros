8b10268e115d900e43a353790838d11169a005b0
nttadress <jortaran@emeal.nttdata.com>
2025-12-11 16:52:57 -0500

    Remove unused SAP parameters insertion from deploy_steps.sql

--- steps/deploy_steps.sql
+++ steps/deploy_steps.sql
@@ -5,8 +5,6 @@
 -- OPS$PROCEDIM: basicamemte es el usaurio propietario
 -- OPS$ADM_ATRSURA, ADM_ATRSURA,  y OPS$SINI: son los usuarios de las aplicaciones de ART
 
-
-
 --***************************************************
 --*  INSERTS (direct connection user mapeoinfo)     *
 --***************************************************
@@ -30,28 +28,6 @@ VALUES ('%', '%', '%', '%', '%', '%', '%', 'CLIENT_ID_ATR', NULL, SYSDATE, NULL,
 INSERT INTO T999_PARAMETROS (CDRAMO, CDSUBRAMO, CDCLAVE1, CDCLAVE2, CDCLAVE3, CDCLAVE4, CDCLAVE5, DSPARAMETRO, PTPARAMETRO, FEALTA, FEBAJA, DSVALOR_PARAMETRO)
 VALUES ('%', '%', '%', '%', '%', '%', '%', 'SECRET_ATR', NULL, SYSDATE, NULL, 'gAAAAABpKiIWKg04wVl5wdMR_G7w4t7DpdkrzG6STwtw_UeTwvd3Ca6mgJr5zebFUy8hvx_f1zjRNxasCun3nskf0U_wAJyz6rYhOI_G9tHhwtU7Q1_JDEcEvbu5Ko5f1cSr2zn_JCdXgk6x0QYza6K1QOSQiQe4QO6_NZQsa3To9eaz9Lzt9xc=');
 commit;
-
--- 2. insert user and password for art siniestros (esto ya no se usa)
-insert into tcob_parametros_sap (
-   cdproceso,
-   cdconsumidor,
-   cdservicio,
-   cdoperacion,
-   cdversion,
-   dsname_space,
-   dspuerto,
-   cdusuario,
-   cdclave
-) values ( 'ART_SINI',
-           'n/a',
-           'sap',
-           'siniestros_cxp',
-           '1.0',
-           'n/a',
-           'n/a',
-           '57W1hRyXRLSiswMg9RSL6DOGymReG9paAKY33CkGBltwBGMz',
-           'gAAAAABo1avf8BCP-W3xacUJPQXwJ9tE9Fqb7i3ifloNOQSxkz-94Yi1Kn77g0FgeWc8Ev30UpwTi4eHZqo_1OKRV8i9xHE2mMT9RzVoWBpmIz8zuPQFKh4qsL6jf5Xasqf72gcyn7_i1yNDD7k2LotUCQzkCPmZbTFgU34YjhY2jxjMkr_M_o4='
-           );
 
 
 --**********************************************************
